4f2b54b8de79d6bec13954d48be4d3696dec74b5
Naresh.R <nareshrajaparimala000@gmail.com>
2025-10-04 10:50:44 +0530

    vercel config detail update

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          utils: ['axios', 'jspdf', 'html2canvas', 'docx', 'file-saver']
+        }
+      }
+    }
+  },
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
   }
 })
